695f680f82e8f2e92e6948469d55bff355cabb26
DESKTOP-Q9LCPOO\Joohan <joohan33@gmail.com>
2018-12-27 22:16:13 +0900

    initial commit

---
+++ src/main/java/lee/joohan/SpringBootSimpleBoard/SpringBootSimpleBoardApplication.java
@@ -0,0 +1,14 @@
+package lee.joohan.SpringBootSimpleBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleBoardApplication.class, args);
+	}
+
+}
+
